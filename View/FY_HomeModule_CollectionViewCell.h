8af025479f99300bc4cfa73e662b0279528b557e
liuyujing <liuyujingmusic@163.com>
2015-10-09 09:22:14 +0800

    create
    Create new project

---
+++ View/FY_HomeModule_CollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FY_HomeModule_CollectionViewCell.h
+//  Funny
+//
+//  Created by Bruce on 15/10/8.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FY_HomeModule_CollectionViewCell : UICollectionViewCell
+
+@end
---
+++ View/FY_HomeModule_CollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  FY_HomeModule_CollectionViewCell.m
+//  Funny
+//
+//  Created by Bruce on 15/10/8.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import "FY_HomeModule_CollectionViewCell.h"
+
+@implementation FY_HomeModule_CollectionViewCell
+
+@end
